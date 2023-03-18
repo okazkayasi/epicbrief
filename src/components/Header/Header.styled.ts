f2e3eba5bc885407cf42364533d4588c2e790ab1
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2023-03-18 23:40:03 +0300

    feat: start

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const SHeaderWrapper = styled.header`
+  background-color: lightgray;
+  padding: 0.625rem;
+`;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { SHeaderWrapper } from "@/components/Header/Header.styled";
+
+export const Header = () => {
+  return (
+    <SHeaderWrapper>
+      <h1>Meetings</h1>
+    </SHeaderWrapper>
+  );
+};
